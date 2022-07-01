af9b5c436fa0a87c1c5845c6d68c66fd5a35c334
rungene <rungenesamuel@gmail.com>
2022-07-01 05:57:30 +0300

    refactor : Updating the Repository.

--- app/src/main/java/com/rungenes/roomwordsample/WordRepository.kt
+++ app/src/main/java/com/rungenes/roomwordsample/WordRepository.kt
@@ -1,15 +1,21 @@
 package com.rungenes.roomwordsample
 
-import androidx.lifecycle.LiveData
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
 
 // Declares the DAO as a private property in the constructor. Pass in the DAO
 // instead of the whole database, because you only need access to the DAO
 class WordRepository(private val wordDao: WordDao) {
 
     // Room executes all queries on a separate thread.
-    // Observed LiveData will notify the observer when the data has changed.
-    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+    // Observed Flow will notify the observer when the data has changed.
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
 
+    // By default Room runs suspend queries off the main thread, therefore, we don't need to
+    // implement anything else to ensure we're not doing long running database work
+    // off the main thread.
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
     suspend fun insert(word: Word) {
         wordDao.insert(word)
     }
