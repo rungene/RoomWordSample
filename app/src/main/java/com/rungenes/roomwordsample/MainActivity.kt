46681613de8cf6c6de5235085c7a350838b82aad
rungene <rungenesamuel@gmail.com>
2022-07-03 19:30:28 +0300

    refactor : Create ViewModel.To create the ViewModel you used the viewModels delegate, passing in an instance of our WordViewModelFactory. This is constructed based on the repository retrieved from the WordsApplication.

--- app/src/main/java/com/rungenes/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/rungenes/roomwordsample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
@@ -15,7 +16,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainActivity : AppCompatActivity() {
 
     private val newWordActivityRequestCode = 1
-    private lateinit var wordViewModel: WordViewModel
+
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
